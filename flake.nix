7bf87a0a146d3e94c7ffa09ddd0479e26874830a
tomoyukim <tomoyukim@outlook.com>
2025-01-13 23:17:19 +0900

    update rust-overlay config in flake.nix to use rustup toolchain.

--- flake.nix
+++ flake.nix
@@ -4,28 +4,37 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlay = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlay; };
-      in {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            openssl # cargo-generate
-            pkg-config
-            probe-rs-tools
-            picoprobe-udev-rules
-            libusb1
-            libudev-zero
-            rust-bin.stable.latest.default
-            rustfmt
-            rust-analyzer
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchain {
+          channel = "stable";
+          components = [
+            "clippy"
+            "rustfmt"
+            "rust-analyzer"
+            "rust-src"
+          ];
+          targets = [
+            "thumbv6m-none-eabi"
           ];
         };
-      }
+      in {
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              probe-rs-tools
+              rustToolchain
+              flip-link
+            ];
+          };
+        }
     );
 }
