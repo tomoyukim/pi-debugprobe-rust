c1101529fa5a459fe7923cc969545e3b7994ae3f
tomoyukim <tomoyukim@outlook.com>
2025-01-13 16:30:29 +0900

    update flake.nix with rust-overlay

--- flake.nix
+++ flake.nix
@@ -4,16 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        overlay = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlay; };
       in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl # cargo-generate
+            pkg-config
+            probe-rs-tools
+            picoprobe-udev-rules
+            libusb1
+            libudev-zero
+            rust-bin.stable.latest.default
             rustfmt
             rust-analyzer
           ];
